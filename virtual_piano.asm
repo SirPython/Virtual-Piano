b7d29ebf091c8a8373c3a6dd5e2dfdce0c6bc4e3
Anthony <anoky96@gmail.com>
2015-04-15 16:44:05 -0400

    initial

---
+++ virtual_piano.asm
@@ -0,0 +1,13 @@
+;===============================================================================
+; Virtual Piano -- a virtual and playable piano
+; By SirPython of Code Review and GitHub
+;
+; virtual_piano.asm
+;===============================================================================
+jmp start
+
+
+
+start:
+
+process
